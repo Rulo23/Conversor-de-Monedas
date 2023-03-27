aa6444e4fe57d9a2ebcbfad69095d87163fa87d5
Rulo23 <123419965+Rulo23@users.noreply.github.com>
2023-03-27 14:43:12 -0600

    Add files via upload

---
+++ ConversionDistancia.java
@@ -0,0 +1,32 @@
+
+package challenge;
+
+public class ConversionDistancia {
+		static	double resultado;
+		static	double nuevoValor;
+		static	double nuevoValor2;
+		 
+	public static Double convertir(String unidad1, String unidad2,double cantidad) {
+			
+			double[] valorUnidad= {1.056970721911E-13,1,1000,100000,1000000,1e+12,0.6213711922,1093.6132983,3280.839895,39370.07874,0.5399568035};
+			String[] Unidad= {"años luz","kilometros","metros","centimetros","milimetros","nanometro","millas","yardas","pies","pulgadas","millas nautica"};
+			
+			for(int i=0;i<=10;i++) {
+				if(unidad1==Unidad[i]) {
+					nuevoValor=cantidad/valorUnidad[i];
+					
+				}
+			
+				if(unidad2==Unidad[i]) {
+					nuevoValor2=valorUnidad[i];
+					
+				}
+				
+			}
+			
+			resultado=nuevoValor2 *nuevoValor;
+			System.out.println(resultado);
+		return resultado;
+		
+	}
+}
---
+++ MainFrame.java
@@ -0,0 +1,33 @@
+
+package challenge;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainFrame() {
+		setTitle("CONVERSOR");
+	}
+
+}
